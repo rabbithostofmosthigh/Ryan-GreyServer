3c2a207057853fcac0529d2814b4135fc66b8024
rabbithostofmosthigh <rabbithostofmosthigh@gmail.com>
2025-11-15 08:03:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "Paydaysite1@gmail.com";
 const pass = "jrqjapmxebtahohb";
-// 9th June
+// 1st dec
 
 // Middleware
 app.use(express.json());
@@ -116,3 +116,4 @@ app.post("/pin", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
